b0fb6dda634f5b2a83d2a47bb15a5fa0da51e71b
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2023-01-03 21:19:25 -0500

    updated

--- Sources/NeuronDatasets/UIHelpers/UIImage+Extensions.swift
+++ Sources/NeuronDatasets/UIHelpers/UIImage+Extensions.swift
@@ -27,6 +27,52 @@ public extension UIImage {
     var b: UInt8
   }
   
+  func asSquareRGBATensor(zeroCenter: Bool = false) -> Tensor {
+    guard let pixelData = self.cgImage?.dataProvider?.data else { return Tensor() }
+    
+    let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+    
+    var rArray: [Float] = []
+    var gArray: [Float] = []
+    var bArray: [Float] = []
+    var aArray: [Float] = []
+
+    for y in 0..<Int(self.size.height) {
+      for x in 0..<Int(self.size.width) {
+        let pos = CGPoint(x: x, y: y)
+        
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y) * 4) + Int(pos.x) * 4)
+        
+        var r = Float(data[pixelInfo])
+        var g = Float(data[pixelInfo + 1])
+        var b = Float(data[pixelInfo + 2])
+        var a = Float(data[pixelInfo + 3])
+
+        if zeroCenter {
+          r = (r - 127.5) / 127.5
+          g = (g - 127.5) / 127.5
+          b = (b - 127.5) / 127.5
+          a = (a - 127.5) / 127.5
+        } else {
+          r = r / 255.0
+          g = g / 255.0
+          b = b / 255.0
+          a = a / 255.0
+        }
+        
+        rArray.append(r)
+        gArray.append(g)
+        bArray.append(b)
+        aArray.append(a)
+      }
+    }
+    
+    return Tensor([rArray.reshape(columns: Int(self.size.width)),
+                   gArray.reshape(columns: Int(self.size.width)),
+                   bArray.reshape(columns: Int(self.size.width)),
+                   aArray.reshape(columns: Int(self.size.width))])
+  }
+  
   func asSquareRGBTensor(zeroCenter: Bool = false) -> Tensor {
     guard let pixelData = self.cgImage?.dataProvider?.data else { return Tensor() }
     
@@ -48,8 +94,8 @@ public extension UIImage {
         
         if zeroCenter {
           r = (r - 127.5) / 127.5
-          g = (r - 127.5) / 127.5
-          b = (r - 127.5) / 127.5
+          g = (g - 127.5) / 127.5
+          b = (b - 127.5) / 127.5
         } else {
           r = r / 255.0
           g = g / 255.0
