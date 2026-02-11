98a0a338c4468db463b4347f01e8a805d7eefa92
William Vabrinskas <wvabrinskas@gmail.com>
2026-02-10 21:47:33 -0500

    point to versioned neuron

--- Package.swift
+++ Package.swift
@@ -17,9 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      //.package(url: "https://github.com/wvabrinskas/Neuron.git", from: "2.0.19"),
-      .package(path: "../Neuron"),
-      .package(path: "../NumSwift"),
+      .package(url: "https://github.com/wvabrinskas/Neuron.git", from: "2.0.19"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
